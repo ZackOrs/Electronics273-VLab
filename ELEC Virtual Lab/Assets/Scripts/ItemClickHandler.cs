ca5da8f1ef8f283248474382dc5b5387283db29d
ZackOrs <zachary_orsoli@hotmail.com>
2021-02-07 19:21:43 -0500

    Working on adding lines for objects

--- ELEC Virtual Lab/Assets/Scripts/ItemClickHandler.cs
+++ ELEC Virtual Lab/Assets/Scripts/ItemClickHandler.cs
@@ -10,11 +10,12 @@ public class ItemClickHandler : MonoBehaviour
 
     public static SpawnableItem spawnableItem;
 
-    [SerializeField] GameObject BreadboardUI;
+    [SerializeField] GameObject _breadboardUI;
+    [SerializeField] Image _wireImage;
 
     public static bool isBBSlotFree = false;
-    private GameObject pointA = null;
-    private GameObject pointB = null;
+    private GameObject _pointA = null;
+    private GameObject _pointB = null;
 
     
     void start()
@@ -109,7 +110,82 @@ public class ItemClickHandler : MonoBehaviour
 
     private void DrawLineBetweenPoints()
     {  
+        ResetWire();
+        // Instantiate(_wireImage,_breadboardUI.transform);
 
+        float distance = Vector2.Distance(_pointA.transform.position,_pointB.transform.position);
+        float rotation = AngleBetweenVector2(_pointA.transform.position,_pointB.transform.position);
+        float posX = PositionXForLine(_pointA.transform.position,_pointB.transform.position);
+        float posY = PositionYForLine(_pointA.transform.position,_pointB.transform.position);
+
+
+        Debug.Log("Rotation is:" + rotation);
+
+        Debug.Log("X is:" + posX);
+        Debug.Log("Y is:" + posY);
+        _wireImage.transform.position = new Vector2(posX,posY);
+        _wireImage.rectTransform.sizeDelta = new Vector2(distance, _wireImage.rectTransform.sizeDelta.y);
+        _wireImage.transform.Rotate(0,0,rotation,Space.World);
+
+        
+        Instantiate(_wireImage,_breadboardUI.transform);
+
+        // //For creating line renderer object
+        // LineRenderer lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
+        // lineRenderer.transform.parent = _breadboardUI.transform;
+        // lineRenderer.material = _lineMaterial;    
+        // lineRenderer.startColor = Color.black;
+        // lineRenderer.endColor = Color.black;
+        // lineRenderer.startWidth = 5.0f;
+        // lineRenderer.endWidth = 5.0f;
+        // lineRenderer.positionCount = 2;
+        // lineRenderer.useWorldSpace = true;    
+
+        // //For drawing line in the world space, provide the x,y,z values
+        // lineRenderer.SetPosition(0, _pointA.transform.position); //x,y and z position of the starting point of the line
+        // lineRenderer.SetPosition(1, _pointB.transform.position); //x,y and z position of the starting point of the line
+
+    }
+
+    private void ResetWire()
+    {
+        _wireImage.transform.position = new Vector3(0,0,0);
+        _wireImage.transform.Rotate(0,0,0,Space.World);
+        _wireImage.rectTransform.sizeDelta = new Vector2(25, _wireImage.rectTransform.sizeDelta.y);
+    }
+
+    private float AngleBetweenVector2(Vector2 vec1, Vector2 vec2)
+    {
+            Vector2 diference = vec2 - vec1;
+            float sign = (vec2.y < vec1.y)? -1.0f : 1.0f;
+            return Vector2.Angle(Vector2.right, diference) * sign;
+    }
+
+    private float PositionXForLine(Vector2 vec1, Vector2 vec2)
+    {
+
+
+
+        float x1 = vec1.x;
+        float x2 = vec2.x;
+        Debug.Log("X1 is:" + x1);
+        Debug.Log("X2 is:" + x2);
+        if(x1 < x2)
+            return x1;
+        else
+            return x2;
+
+    }
+
+    private float PositionYForLine(Vector2 vec1, Vector2 vec2)
+    {
+        float y1 = vec1.y - 500;
+        float y2 = vec2.y - 500;
+
+        if(y1 > y2)
+            return y1;
+        else
+            return y2;
     }
 
 
@@ -130,11 +206,11 @@ public class ItemClickHandler : MonoBehaviour
 
                     if(Globals.mouseClickAction == Globals.MouseClickAction.TwoClicks_FirstClick)
                     {
-                        pointA = results[i].gameObject;
+                        _pointA = results[i].gameObject;
                     }
                     else if(Globals.mouseClickAction == Globals.MouseClickAction.TwoClicks_SecondClick)
                     {
-                        pointB = results[i].gameObject;
+                        _pointB = results[i].gameObject;
                     }
                 }
             }
