76a95ba7c7791e284dcc0a4da631d0bb85784e6d
saradohakhan <60189153+saradohakhan@users.noreply.github.com>
2021-02-09 19:59:44 -0500

    Fix Object + Create cs files
    Made the object child to parents and create cs files for resistor spawn

---
+++ ELEC Virtual Lab/Assets/Scripts/SpawnableItems/ResistorSpawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+
+
+public class ResistorSpawn : SpawnableItemBase
+{
+    public ResistorSpawn()
+    {
+    }
+}
