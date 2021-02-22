cd715cd080fcf48afbd0d8f42df3ede9aa0949f7
saradohakhan <60189153+saradohakhan@users.noreply.github.com>
2021-02-22 04:17:46 -0500

    Power Supply
    Selectable. Need Ui Canvas

---
+++ ELEC Virtual Lab/Assets/Scripts/SelectableObjects/PowerSupplySelect.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PowerSupplySelect : SelectableItemBase
+{
+    [SerializeField] private GameObject PowerSupplyPanel = null;
+    // Start is called before the first frame update
+
+    public override string Name
+    {
+        get
+        {
+            return "Power Supply";
+        }
+    }
+
+    public override void onInteract()
+    {
+        OpenPowerSupplyPanel();
+    }
+
+    public void OpenPowerSupplyPanel()
+    {
+       PowerSupplyPanel.SetActive(true);
+
+        Globals.showCrosshair = false;
+        Globals.menuOpened = true;
+        Globals.showCrosshair = false;
+
+        Time.timeScale = 0.0f;
+        UnityEngine.Cursor.lockState = CursorLockMode.None;
+        UnityEngine.Cursor.visible = true;
+
+    }
+
+    private void ClosePanel()
+    {
+        PowerSupplyPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p") || Input.GetKeyDown(KeyCode.Escape))
+        {
+            ClosePanel();
+        }
+    }
+}
