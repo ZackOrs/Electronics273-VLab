c4f5bf1f339060129eef9309166e79eb66811bc7
Nabil Alamgir <nabilmdude@hotmail.com>
2021-03-20 19:06:36 -0400

    online functionalities
    online db works
    login time logout time
    class management done

---
+++ ELEC Virtual Lab/Assets/Scripts/DisplayTime.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayTime : MonoBehaviour
+{
+    public GameObject timeDisplay;
+    public int hour;
+    public int minute;
+    public int second;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void showTime()
+    {  
+        {
+            hour = System.DateTime.Now.Hour;
+            minute = System.DateTime.Now.Minute;
+            second = System.DateTime.Now.Second;
+            timeDisplay.GetComponent<Text>().text = "Time: " + hour + ":" + minute + ":" + second;
+        }
+
+    }
+}
--- ELEC Virtual Lab/Assets/Scripts/UnityLoginLogoutRegister.cs
+++ ELEC Virtual Lab/Assets/Scripts/UnityLoginLogoutRegister.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,8 +8,14 @@ using UnityEngine.Networking;
 using TMPro;
 
 
+
 public class UnityLoginLogoutRegister : MonoBehaviour
 {
+
+    public GameObject timeDisplay;
+    public int hour;
+    public int minute;
+    public int second;
 
     public string baseUrl = "http://localhost:8888/UnityLoginLogoutRegister/";
       
@@ -17,31 +24,44 @@ public class UnityLoginLogoutRegister : MonoBehaviour
     public InputField accountSID;
     public InputField accountclassCode;
     public InputField accountLabSection;
+    public InputField taUsername;
+    public InputField taPassword;
     public TMP_Text info;
 
-    private string currentName;
+    private string currentName; //student and TA
     private string ukey = "accountname";
+    private string taKey = "taAccount";
 
     // Start is called before the first frame update
     void Start()
     {
         currentName = "";
 
-        if(PlayerPrefs.HasKey(ukey))
+        if (PlayerPrefs.HasKey(ukey))
         {
-            if (PlayerPrefs.GetString(ukey) != "") {
+            if (PlayerPrefs.GetString(ukey) != "")
+            {
                 currentName = PlayerPrefs.GetString(ukey);
-                info.text = "You are logged in as: " + currentName;
+             //   info.text = "You are logged in as: " + currentName;
             }
+            else
+          if (PlayerPrefs.HasKey(taKey)) //DOUBLE CHECK
+            {
+                if (PlayerPrefs.GetString(taKey) != "")
+                {
+                    currentName = PlayerPrefs.GetString(taKey);
+                    info.text = "You are logged in as: " + currentName;
+                }
+                else
+                {
+                    info.text = "You are not logged in. ";
+                }
+
+            } // DOUBLE CHECK
             else
             {
                 info.text = "You are not logged in. ";
             }
-
-        }
-        else
-        {
-            info.text = "You are not logged in. ";
         }
     }
 
@@ -54,7 +74,7 @@ public class UnityLoginLogoutRegister : MonoBehaviour
     public void AccountLogout()
     {
         currentName = "";
-        PlayerPrefs.SetString(ukey, currentName);
+        PlayerPrefs.SetString(ukey, currentName); // do if statement for student or TA
         info.text = "You are now logged out.";
     }
 
@@ -74,7 +94,24 @@ public class UnityLoginLogoutRegister : MonoBehaviour
         string cCode = accountclassCode.text;
         string lSection = accountLabSection.text;
         StartCoroutine(LogInAccount(sName, sID, cCode, lSection));
+        //StartCoroutine(ChangeScene(1));
     }
+
+    public void taAccountLogin()
+    {
+        string taName = taUsername.text;
+        string taPword = taPassword.text;
+        StartCoroutine(taLogInAccount(taName, taPword));
+    }
+
+    public void DeleteAccount()
+     {
+        string sName = accountName.text;
+        string sID = accountSID.text;
+        string cCode = accountclassCode.text;
+        string lSection = accountLabSection.text;
+        StartCoroutine(DeleteAccount(sName, sID, cCode, lSection));
+     }
 
     IEnumerator RegisterNewAccount(string sName, string sID, string cCode, string lSection)
     {
@@ -113,6 +150,7 @@ public class UnityLoginLogoutRegister : MonoBehaviour
             www.downloadHandler = new DownloadHandlerBuffer();
             yield return www.SendWebRequest();
 
+
             if (www.isNetworkError)
             {
                 Debug.Log(www.error);
@@ -123,7 +161,44 @@ public class UnityLoginLogoutRegister : MonoBehaviour
                 if(responseText == "1")
                 {
                     PlayerPrefs.SetString(ukey, sName);
-                    SceneManager.LoadScene(1);
+                    //info.text = "Login Success with Name: " + sName;
+
+                    StartCoroutine(ChangeScene(1));
+                    hour = System.DateTime.Now.Hour;
+                    minute = System.DateTime.Now.Minute;
+                    second = System.DateTime.Now.Second;
+                    timeDisplay.GetComponent<Text>().text = "Time: " + hour + ":" + minute + ":" + second;
+                }
+                else
+                {
+                    info.text = "Login Failed. Please try again";
+                    
+                }
+            }
+        }
+    }
+
+    IEnumerator taLogInAccount(string taName, string taPword)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("tausername", taName);
+        form.AddField("tapassword", taPword);
+        using (UnityWebRequest www = UnityWebRequest.Post(baseUrl, form))
+        {
+            www.downloadHandler = new DownloadHandlerBuffer();
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                string responseText = www.downloadHandler.text;
+                if (responseText == "1")
+                {
+                    PlayerPrefs.SetString(taKey, taName);
+                    SceneManager.LoadScene(3);
                     //info.text = "Login Success with Name: " + sName;
                 }
                 else
@@ -132,5 +207,39 @@ public class UnityLoginLogoutRegister : MonoBehaviour
                 }
             }
         }
+    }
+
+    IEnumerator DeleteAccount(string sName, string sID, string cCode, string lSection)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("Deletename", sName);
+        form.AddField("Deletestudentid", sID);
+        form.AddField("Deleteclasscode", cCode);
+        form.AddField("Deletelabsection", lSection);
+        using (UnityWebRequest www = UnityWebRequest.Post(baseUrl, form))
+        {
+            www.downloadHandler = new DownloadHandlerBuffer();
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                string responseText = www.downloadHandler.text;
+                if (responseText == "0")
+                {
+                    Debug.Log("Response = " + responseText);
+                    info.text = "Student removed successfully" + responseText;
+                }
+            }
+        }
+    }
+
+    IEnumerator ChangeScene(int index, float delay = 3f)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(1);
     }
 }
