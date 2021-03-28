e3097ab405c6ed5c3c58df84b7a77d2a903f6ff4
kenkire <60125004+kenkire@users.noreply.github.com>
2021-03-28 17:40:40 -0400

    Graph

---
+++ ELEC Virtual Lab/Assets/Scripts/Graph.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab = default;
+
+    [SerializeField, Range(1,1000)]
+    int resolution = 1;
+
+    Transform[] points;
+
+    void Awake()
+    {
+        float step = 2f/resolution;
+        var position = Vector3.zero;
+        var scale = Vector3.one * step;
+        points = new Transform[resolution];
+		for (int i = 0; i < resolution; i++) {
+			Transform point = Instantiate(pointPrefab);
+			position.x = (i + 0.5f) * step - 1f;
+			point.localPosition = position;
+			point.localScale = scale;
+            point.SetParent(transform,false);
+            points[i] = point;
+
+		}
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.time;
+        for (int i = 0; i< points.Length; i++){
+			Transform point = points[i];
+			Vector3 position = point.localPosition;
+			position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+
+            //y = sin(pi*(x+t));
+			point.localPosition = position;
+        }
+    }
+}
---
+++ ELEC Virtual Lab/Assets/Scripts/UIGridRenderer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGridRenderer : Graphic
+{
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        vh.Clear();
+
+        float width = rectTransform.rect.width;
+        float height = rectTransform.rect.height;
+
+        UIVertex vertex = UIVertex.simpleVert;
+        vertex.color = color;
+
+        vertex.position = new Vector3(0,0);
+        vh.AddVert(vertex);
+
+        vertex.position = new Vector3(0, height);
+        vh.AddVert(vertex);
+
+        vertex.position = new Vector3(width,height);
+        vh.AddVert(vertex);
+
+        vertex.position = new Vector3(width,0);
+        vh.AddVert(vertex);
+
+        vh.AddTriangle(0,1,2);
+        vh.AddTriangle(2,3,0);
+    }
+
+}
