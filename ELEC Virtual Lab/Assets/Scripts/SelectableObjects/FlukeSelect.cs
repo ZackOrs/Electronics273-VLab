be8f04f500606edb35cef17cd45916567142cee3
kenkire <60125004+kenkire@users.noreply.github.com>
2021-03-30 15:54:22 -0400

    CameraScript

--- ELEC Virtual Lab/Assets/Scripts/SelectableObjects/FlukeSelect.cs
+++ ELEC Virtual Lab/Assets/Scripts/SelectableObjects/FlukeSelect.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+
 public class FlukeSelect : SelectableItemBase
 {
-   // [SerializeField] private string spawnableTag = "Spawnable";
+    // [SerializeField] private string spawnableTag = "Spawnable";
 
-    public GameObject FlukePanel;
+    public GameObject Camera;
+    [SerializeField] GameObject focusPoint = null;
 
     public override string Name
     {
@@ -20,49 +22,14 @@ public class FlukeSelect : SelectableItemBase
     
     public override void onInteract()
     {
-        OpenFlukePanel();
+        Globals.lookingAtFocusableObject = true;
+        Camera.GetComponent<AnimateCamera>().targetObject = focusPoint;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown("p") || Input.GetKeyDown(KeyCode.Escape))
-        {
-
-        }
-    }
-
-    public void OpenFlukePanel()
-    {
-        FlukePanel.SetActive(true);
-
-        Globals.showCrosshair = false;
-        Globals.menuOpened = true;
-        Globals.showCrosshair = false;
-
-        Time.timeScale = 0.0f;
-        UnityEngine.Cursor.lockState = CursorLockMode.None;
-        UnityEngine.Cursor.visible = true;
-
-    }
-    
-    public void CloseFlukePanel()
-    {
-
-        FlukePanel.SetActive(false);
-
-        Globals.showCrosshair = true;
-        Globals.menuOpened = false;
-        Globals.mouseClickAction = Globals.MouseClickAction.NoClick;
-
-        Time.timeScale = 1.0f;
-        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
-        UnityEngine.Cursor.visible = false;
 
     }
 
-    public void CancelButton()
-    {
-        CloseFlukePanel();
-    }
 
 }
--- ELEC Virtual Lab/Assets/Scripts/SelectableObjects/OscilloscopeSelect.cs
+++ ELEC Virtual Lab/Assets/Scripts/SelectableObjects/OscilloscopeSelect.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+
 public class OscilloscopeSelect : SelectableItemBase
 {
-     // [SerializeField] private string spawnableTag = "Spawnable";
+    // [SerializeField] private string spawnableTag = "Spawnable";
 
-    public GameObject OscilloscopePanel;
+    public GameObject Camera;
+    [SerializeField] GameObject focusPoint = null;
 
     public override string Name
     {
@@ -20,50 +22,14 @@ public class OscilloscopeSelect : SelectableItemBase
     
     public override void onInteract()
     {
-        OpenOscilloscopePanel();
+        Globals.lookingAtFocusableObject = true;
+        Camera.GetComponent<AnimateCamera>().targetObject = focusPoint;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown("p") || Input.GetKeyDown(KeyCode.Escape))
-        {
-
-        }
-    }
-
-    public void OpenOscilloscopePanel()
-    {
-        OscilloscopePanel.SetActive(true);
-
-        Globals.showCrosshair = false;
-        Globals.menuOpened = true;
-        Globals.showCrosshair = false;
-
-        Time.timeScale = 0.0f;
-        UnityEngine.Cursor.lockState = CursorLockMode.None;
-        UnityEngine.Cursor.visible = true;
-
-    }
-    
-    public void CloseOscilloscopePanel()
-    {
-
-        OscilloscopePanel.SetActive(false);
-
-        Globals.showCrosshair = true;
-        Globals.menuOpened = false;
-        Globals.mouseClickAction = Globals.MouseClickAction.NoClick;
-
-        Time.timeScale = 1.0f;
-        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
-        UnityEngine.Cursor.visible = false;
 
     }
 
-    public void CancelButton()
-    {
-        CloseOscilloscopePanel();
-    }
 
 }
-
